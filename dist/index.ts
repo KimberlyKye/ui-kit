5acf8d8ea81396bdbca1d504752ee2449d85dc6e
Melanie <lovelystasik@gmail.com>
2024-01-23 10:25:16 +0300

    copy index

---
+++ dist/index.ts
@@ -0,0 +1,20 @@
+import { defineCustomElement } from 'vue';
+
+const MyComponent = {
+  template: `<div>
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <p>test vue component</p>
+    </div>`,
+  props: {
+    title: String,
+    description: String,
+  },
+};
+
+const MyComponentWC = customElements.define(
+  'my-component',
+  defineCustomElement(MyComponent)
+);
+
+export default MyComponentWC;
